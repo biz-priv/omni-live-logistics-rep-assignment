55a557351ac0540d02b59cc82706a45088925b11
biz-cloud-abhishek <120293411+abhishekBCE@users.noreply.github.com>
2023-09-22 12:09:33 -0500

    dynamo functions copied

--- src/establish-metrics.py
+++ src/establish-metrics.py
@@ -2,8 +2,12 @@ import datetime
 import dateutil.tz
 import requests
 import os
-from shared.dynamo import put_item, get_table
+# from shared.dynamo import put_item, get_table
 # import pytz
+
+import boto3
+
+dynamodb = boto3.client('dynamodb', region_name='us-east-1')
 
 def handler(event, context):
     list_of_users = ['bednchr','browaus','casajak','cruzale','elizmau','hodgbri','pilczac','ruizrob','schmric']
@@ -95,3 +99,49 @@ def handler(event, context):
         put_item(table_obj, userRecord)
 
     return "Function ran successfully"
+
+
+# Get a reference to an existing table
+def get_table(table_name):
+    table = dynamodb.Table(table_name)
+    return table
+
+# Put an item into a table
+def put_item(table, item):
+    response = table.put_item(Item=item)
+    print("PutItem succeeded:", response)
+
+# Get an item from a table using its primary key
+def get_item(table, key):
+    response = table.get_item(Key=key)
+    item = response.get('Item')
+    return item
+
+# Update an item in a table
+def update_item(table, key, update_expression, expression_attribute_values):
+    response = table.update_item(
+        Key=key,
+        UpdateExpression=update_expression,
+        ExpressionAttributeValues=expression_attribute_values
+    )
+    print("UpdateItem succeeded:", response)
+
+# Delete an item from a table using its primary key
+def delete_item(table, key):
+    response = table.delete_item(Key=key)
+    print("DeleteItem succeeded:", response)
+
+# Query items in a table
+def query_items(table, key_condition_expression, expression_attribute_values):
+    response = table.query(
+        KeyConditionExpression=key_condition_expression,
+        ExpressionAttributeValues=expression_attribute_values
+    )
+    items = response.get('Items', [])
+    return items
+
+# Scan all items in a table
+def scan_table(table):
+    response = table.scan()
+    items = response.get('Items', [])
+    return items
