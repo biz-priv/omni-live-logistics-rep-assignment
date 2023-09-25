535581dd7341923ddd330d9025495d93783545f2
biz-cloud-abhishek <120293411+abhishekBCE@users.noreply.github.com>
2023-09-25 12:20:10 -0500

    exception handling added to apis

--- src/shared/api.py
+++ src/shared/api.py
@@ -8,26 +8,46 @@ mcleod_headers = {'Accept': 'application/json',
                 'Content-Type': 'application/json'}
 
 def searchMovementByUser(user_id):
-    url = f"{base_url}/api/movements/search?movement.dispatcher_user_id={user_id}&status=D&orderBy=destination.actual_arrival+DESC&recordLength=50"
-    response = requests.get(url, auth=(username, password), headers=mcleod_headers)
-    return response
+    try:
+        url = f"{base_url}/api/movements/search?movement.dispatcher_user_id={user_id}&status=D&orderBy=destination.actual_arrival+DESC&recordLength=50"
+        response = requests.get(url, auth=(username, password), headers=mcleod_headers)
+        return response
+    except Exception as error:
+        print(f"Error at searchMovementByUser, user_id - {user_id}")
+        raise
 
 def callins(move_id):
-    url = f"https://tms-lvlp.loadtracking.com/ws/api/callins/M/{move_id}"
-    response = requests.get(url, auth=(username, password), headers=mcleod_headers)
-    return response
+    try:
+        url = f"https://tms-lvlp.loadtracking.com/ws/api/callins/M/{move_id}"
+        response = requests.get(url, auth=(username, password), headers=mcleod_headers)
+        return response
+    except Exception as error:
+        print(f"Error at callins, move_id - {move_id}")
+        raise
         
 def searchParadeLoads():
-    url = f"{base_url}/api/movements/search?dispatcher_user_id=parade&status=<>V"
-    response = requests.get(url, auth=(username, password), headers=mcleod_headers)
-    return response
+    try:
+        url = f"{base_url}/api/movements/search?dispatcher_user_id=parade&status=<>V"
+        response = requests.get(url, auth=(username, password), headers=mcleod_headers)
+        return response
+    except Exception as error:
+        print(f"Error at searchParadeLoads")
+        raise
 
 def getMovementById(movement_id):
-    url = f"{base_url}/movement/{movement_id}"
-    response = requests.get(url, auth=(username, password), headers=mcleod_headers)
-    return response
+    try:
+        url = f"{base_url}/movement/{movement_id}"
+        response = requests.get(url, auth=(username, password), headers=mcleod_headers)
+        return response
+    except Exception as error:
+        print(f"Error at getMovementById, movement_id - {movement_id}")
+        raise
 
 def updateMovement(json):
-    url = f"{base_url}/movement/update"
-    response = requests.put(url, auth=(username, password), headers=mcleod_headers,json=json)
-    return response
+    try:    
+        url = f"{base_url}/movement/update"
+        response = requests.put(url, auth=(username, password), headers=mcleod_headers,json=json)
+        return response
+    except Exception as error:
+        print(f"Error at updateMovement, json - {json}")
+        raise
