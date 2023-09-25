9bcc1da6d6c8e609e6d21e95e1dcebb4fa71aeaf
biz-cloud-abhishek <120293411+abhishekBCE@users.noreply.github.com>
2023-09-25 09:15:39 -0500

    Update update-dispatcher.py

--- src/update-dispatcher.py
+++ src/update-dispatcher.py
@@ -2,10 +2,13 @@ import datetime
 import requests
 import os
 import sys
+from boto3.dynamodb.conditions import Key, Attr
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
 from shared.api import searchParadeLoads, getMovementById, updateMovement
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 def handler(event, context):
     find_parade_loads()
@@ -42,3 +45,25 @@ def update_dispatcher(movement_id, new_user):
     except Exception as error:
         print(error)
         return "Error - update_dispatcher"
+    
+def scan_users():
+    users = []
+    table = dynamodb.Table(os.environ('USER_METRICS_TABLE'))
+    scan_kwargs = {
+        'FilterExpression': Attr('qualified').eq('true')
+    }
+    try:
+        done = False
+        start_key = None
+        while not done:
+            if start_key:
+                scan_kwargs['ExclusiveStartKey'] = start_key
+            response = table.scan(**scan_kwargs)
+            users.extend(response.get('Items', []))
+            start_key = response.get('LastEvaluatedKey', None)
+            done = start_key is None
+    except Exception as err:
+        print("Exception in scaning users")
+        raise
+
+    return users
